9bbadb4a48a5912c10a2d81aeaec31e7ffbca952
Tomasz Ogonowski <tomasz.ogonowski@visionect.io>
2022-05-15 14:49:11 +0200

    create expenses template

---
+++ src/templates/ExpensesTemplate/index.tsx
@@ -0,0 +1,16 @@
+import { ExpensesTable, ExpensesSum } from './layouts';
+import { ExpensesForm, EurConversionForm } from 'forms';
+import { Headline1 } from 'components';
+import styles from './style.module.scss';
+
+export const ExpensesTemplate = () => {
+  return (
+    <div className={styles.expensesContainer} >
+      <Headline1>List of expenses</Headline1>
+      <EurConversionForm />
+      <ExpensesForm />
+      <ExpensesTable />
+      <ExpensesSum />
+    </div>
+  )
+};
